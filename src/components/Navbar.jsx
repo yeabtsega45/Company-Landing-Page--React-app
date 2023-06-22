559a6bcced3f42d05eb4160ef1b4fd7772fa9dad
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-22 14:39:29 +0300

    creating navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+const Navbar = () => {
+  return (
+    <div>
+      <MenuIcon />
+      <XIcon />
+    </div>
+  );
+};
+
+export default Navbar;
