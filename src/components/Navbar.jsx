4b2356c42f4b8b05f0587d05ae2247bf56d162bb
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-22 21:57:34 +0300

    configuring screen divisions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
-        <div>
-          <h1>BRAND.</h1>
+        <div className="flex items-center">
+          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
         </div>
       </div>
       <MenuIcon className="w-5" />
