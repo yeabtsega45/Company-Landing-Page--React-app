ae64665e41901c55d77f7d52dd6ddd7a260d5d9b
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-23 17:35:50 +0300

    working on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,6 +22,27 @@ const Hero = () => {
         <div>
           <img className="w-full" src={bgImg} alt="/" />
         </div>
+        <div
+          className="absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
+            mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
+            border border-slate-300 rounded-xl text-center shadow-xl"
+        >
+          <p>Data Services</p>
+          <div className="flex justify-between flex-wrap px-4">
+            <p className="flex px-4 py-2 text-slate-500">
+              <CloudUploadIcon className="h-6 text-indigo-600" /> App Security
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <DatabaseIcon className="h-6 text-indigo-600" /> Dashboard Design
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <ServerIcon className="h-6 text-indigo-600" /> Cloud Data
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <PaperAirplaneIcon className="h-6 text-indigo-600" /> API
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
