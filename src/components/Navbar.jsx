3a0535dc389ff1f53256f7155eab0b66bfe383ba
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-23 16:13:44 +0300

    creating hero component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  // const handleClick = setNav(!nav);
+
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
@@ -21,19 +24,18 @@ const Navbar = () => {
           </button>
           <button className="px-8 py-3">Sign Up</button>
         </div>
-        <div className="md:hidden">
-          <MenuIcon className="w-5" />
-          <XIcon className="w-5" />
+        <div className="md:hidden" onClick={(e) => setNav(!nav)}>
+          {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
         </div>
       </div>
-      <ul className="absolute bg-zinc-200 w-full px-8">
+      <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
         <li className="border-b-2 border-zinc-300 w-full">Home</li>
         <li className="border-b-2 border-zinc-300 w-full">About</li>
         <li className="border-b-2 border-zinc-300 w-full">Support</li>
         <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
         <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
         <div className="flex flex-col my-4">
-          <button className="bg-transparent text-indigo-600 px-8 py-3 mr-4">
+          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
             Sign In
           </button>
           <button className="px-8 py-3">Sign Up</button>
