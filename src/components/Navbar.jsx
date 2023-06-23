c549029c605e2740d234b0acdf613105df105531
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-23 11:49:02 +0300

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,17 @@ const Navbar = () => {
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
+          <ul className="hidden md:flex">
+            <li>Home</li>
+            <li>About</li>
+            <li>Support</li>
+            <li>Platforms</li>
+            <li>Pricing</li>
+          </ul>
         </div>
       </div>
       <MenuIcon className="w-5" />
-      <XIcon />
+      <XIcon className="w-5" />
     </div>
   );
 };
