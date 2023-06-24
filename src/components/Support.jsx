9bcde96ced3d641b8d73a4dd9028df602b827206
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-24 22:15:13 +0300

    working on support page

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import supportImg from "../assets/support.jpg";
+import { PhoneIcon, ArrowSmRightIcon } from "@heroicons/react/outline";
 
 const Support = () => {
   return (
@@ -26,17 +27,24 @@ const Support = () => {
             et cum vel?
           </p>
         </div>
-        <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
-              <div className='bg-white rounded-xl shadow-2xl'>
-                  <div className='p-8'>
-                      <PhoneIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
-                      <h3 className='font-bold text-2xl my-6'>Sales</h3>
-                      <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
-                  </div>
-                  <div className='bg-slate-100 pl-8 py-4'>
-                      <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2' /></p>
-                  </div>
-              </div>
+        <div className="grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black">
+          <div className="bg-white rounded-xl shadow-2xl">
+            <div className="p-8">
+              <PhoneIcon className="w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]" />
+              <h3 className="font-bold text-2xl my-6">Sales</h3>
+              <p className="text-gray-600 text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi provident iure placeat blanditiis ea sint earum hic
+                iste quibusdam exercitationem.
+              </p>
+            </div>
+            <div className="bg-slate-100 pl-8 py-4">
+              <p className="flex items-center text-indigo-600">
+                Contact Us <ArrowSmRightIcon className="w-5 ml-2" />
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
