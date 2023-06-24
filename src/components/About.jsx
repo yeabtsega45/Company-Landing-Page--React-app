1f8147ebee271eda6b161ddb826bb906fc5d7991
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-24 21:52:59 +0300

    working on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,6 +13,20 @@ const About = () => {
             asperiores earum placeat veritatis dignissimos itaque.
           </p>
         </div>
+        <div className="grid md:grid-cols-3 gap-1 px-2 text-center">
+          <div className="border py-8 rounded-xl shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">100%</p>
+            <p className="text-gray-400 mt-2">Completion</p>
+          </div>
+          <div className="border py-8 rounded-xl shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">24/7</p>
+            <p className="text-gray-400 mt-2">Delivery</p>
+          </div>
+          <div className="border py-8 rounded-xl shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">100K</p>
+            <p className="text-gray-400 mt-2">Transactions</p>
+          </div>
+        </div>
       </div>
     </div>
   );
