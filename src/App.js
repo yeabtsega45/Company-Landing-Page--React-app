39c65d6e8ff82f2325d909b3fba08887e1d49cdc
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-25 18:08:34 +0300

    creating Pricing card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Support from "./components/Support";
+import AllInOne from "./components/AllInOne";
+import Pricing from "./components/Pricing";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Hero />
       <About />
       <Support />
+      <AllInOne />
+      <Pricing />
     </>
   );
 }
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div name="pricing" className="w-full text-white my-24">
+      <div className="w-full h-[800px] bg-slate-900 absolute mix-blend-overlay"></div>
+
+      <div className="max-w-[1240px] mx-auto py-12">
+        <div className="text-center py-8 text-slate-300">
+          <h2 className="text-3xl uppercase">Pricing</h2>
+          <h3 className="text-5xl font-bold text-white py-8">
+            The right price for your research.
+          </h3>
+          <p className="text-3xl">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+            laudantium odio ullam inventore aliquid ipsum quasi tenetur velit
+            voluptatum iste.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
