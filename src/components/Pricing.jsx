1e2a304809be5c0a10898687993bb55cca9add32
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-25 18:12:36 +0300

    creating Pricing card

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CheckIcon } from "@heroicons/react/solid";
 
 const Pricing = () => {
   return (
@@ -16,6 +17,48 @@ const Pricing = () => {
             laudantium odio ullam inventore aliquid ipsum quasi tenetur velit
             voluptatum iste.
           </p>
+        </div>
+
+        <div className="grid md:grid-cols-2">
+          <div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+            <span className="uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm">
+              Standard
+            </span>
+            <div>
+              <p className="text-6xl font-bold py-4 flex">
+                $49
+                <span className="text-xl text-slate-500 flex flex-col justify-end">
+                  /mo
+                </span>
+              </p>
+            </div>
+            <p className="text-2xl py-8 text-slate-500">
+              Lorem ipsum dolor, sit amet consectetur adipisicing.
+            </p>
+            <div className="text-2xl">
+              <p className="flex py-4">
+                <CheckIcon className="w-8 mr-5 text-green-600" />
+                Lorem, ipsum dolor.
+              </p>
+              <p className="flex py-4">
+                <CheckIcon className="w-8 mr-5 text-green-600" />
+                Lorem, ipsum dolor.
+              </p>
+              <p className="flex py-4">
+                <CheckIcon className="w-8 mr-5 text-green-600" />
+                Lorem, ipsum dolor.
+              </p>
+              <p className="flex py-4">
+                <CheckIcon className="w-8 mr-5 text-green-600" />
+                Lorem, ipsum dolor.
+              </p>
+              <p className="flex py-4">
+                <CheckIcon className="w-8 mr-5 text-green-600" />
+                Lorem, ipsum dolor.
+              </p>
+              <button className="w-full py-4 my-4">Get Started</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
